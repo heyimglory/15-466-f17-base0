1b40bb09db77e3653d71bbb17c4f0a7eb7009398
Glory_Chen <jlps91060@gmail.com>
2017-09-02 11:52:44 -0400

    the blocks can move around and the game could end

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@
 
 #include <chrono>
 #include <iostream>
+#include <ctime>
 
 int main(int argc, char **argv) {
 	//Configuration:
 	struct {
-		std::string title = "Game0: Tennis For One";
-		glm::uvec2 size = glm::uvec2(640, 480);
+		std::string title = "Game0: Shepherd Dog";
+		glm::uvec2 size = glm::uvec2(550, 550);
 	} config;
 
 	//------------  initialization ------------
@@ -75,15 +76,117 @@ int main(int argc, char **argv) {
 	SDL_ShowCursor(SDL_DISABLE);
 
 	//------------  game state ------------
-
+	#define NONE 0
+	#define SHEEP 1
+	#define SHEPHERD 2
+	#define RIGHT 1
+	#define UP 2
+	#define LEFT 3
+	#define DOWN 4
+	
+	class Sheep {
+		private:
+			glm::vec2 init_location;
+			glm::vec2 location;
+			glm::vec2 direction;
+			float speed;
+			int collide_type;
+			int collide_direction;
+			void turn() {
+				if(collide_type = SHEEP)
+				{
+					switch(collide_direction) {
+						case RIGHT: {
+							direction.x = -1;
+							direction.y = 0;
+							break;
+						}
+						case UP: {
+							direction.x = 0;
+							direction.y = -1;
+							break;
+						}
+						case LEFT: {
+							direction.x = 1;
+							direction.y = 0;
+							break;
+						}
+						case DOWN: {
+							direction.x = 0;
+							direction.y = 1;
+							break;
+						}
+					}
+				}
+				else if(collide_type = SHEPHERD) {
+					direction.x *= -1;
+					direction.y *= -1;
+				}
+			}
+		public:
+		Sheep() {};
+		Sheep(float x, float y) {
+			init_location = glm::vec2(x, y);
+			//std::srand((unsigned)std::time(0));
+			switch(std::rand() % 4) {
+				case 0: {
+					direction.x = 1;
+					direction.y = 0;
+					break;
+				}
+				case 1: {
+					direction.x = 0;
+					direction.y = 1;
+					break;
+				}
+				case 2: {
+					direction.x = -1;
+					direction.y = 0;
+					break;
+				}
+				case 3: {
+					direction.x = 0;
+					direction.y = -1;
+					break;
+				}
+			}
+			initSheep();
+		}
+		void initSheep() {
+			location.x = init_location.x;
+			location.y = init_location.y;
+			speed = 0.001f;
+			collide_type = NONE;
+			collide_direction = NONE;
+		}
+		void move() {
+			location.x += speed * direction.x;
+			location.y += speed * direction.y;
+		}
+		glm::vec2 getLocation() {
+			return location;
+		}
+		bool OutOfArea() {
+			if(location.x>=0.85f || location.x<=-0.85f || location.y>=0.85f || location.y<=-0.85f) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		void detectCollide() {};
+	};
 	glm::vec2 mouse = glm::vec2(0.0f, 0.0f);
-	glm::vec2 ball = glm::vec2(0.0f, 0.0f);
-	glm::vec2 ball_velocity = glm::vec2(0.5f, 0.5f);
+	//glm::vec2 ball = glm::vec2(0.0f, 0.0f);
+	//glm::vec2 ball_velocity = glm::vec2(0.5f, 0.5f);
 
 	//------------  game loop ------------
 
 	auto previous_time = std::chrono::high_resolution_clock::now();
 	bool should_quit = false;
+	bool play = false;
+	bool alive = true;
+	Sheep sheep1(0.0f, 0.5f), sheep2(-0.5f, 0.1f), sheep3(0.5f, 0.1f), sheep4(0.3f, -0.4f), sheep5(-0.3f, -0.4f);
 	while (true) {
 		static SDL_Event evt;
 		while (SDL_PollEvent(&evt) == 1) {
@@ -92,8 +195,10 @@ int main(int argc, char **argv) {
 				mouse.x = (evt.motion.x + 0.5f) / float(config.size.x) * 2.0f - 1.0f;
 				mouse.y = (evt.motion.y + 0.5f) / float(config.size.y) *-2.0f + 1.0f;
 			} else if (evt.type == SDL_MOUSEBUTTONDOWN) {
-				ball = mouse;
-				ball_velocity = glm::vec2(0.5f, 0.5f);
+				play = true;
+				alive = true;
+				//ball = mouse;
+				//ball_velocity = glm::vec2(0.5f, 0.5f);
 			} else if (evt.type == SDL_KEYDOWN && evt.key.keysym.sym == SDLK_ESCAPE) {
 				should_quit = true;
 			} else if (evt.type == SDL_QUIT) {
@@ -102,33 +207,59 @@ int main(int argc, char **argv) {
 			}
 		}
 		if (should_quit) break;
+		
+		if(alive) {
+			auto current_time = std::chrono::high_resolution_clock::now();
+			float elapsed = std::chrono::duration< float >(current_time - previous_time).count();
+			previous_time = current_time;
 
-		auto current_time = std::chrono::high_resolution_clock::now();
-		float elapsed = std::chrono::duration< float >(current_time - previous_time).count();
-		previous_time = current_time;
+			{ //update game state:
+				sheep1.move();
+				sheep2.move();
+				sheep3.move();
+				sheep4.move();
+				sheep5.move();
+				if(sheep1.OutOfArea() || sheep2.OutOfArea() || sheep3.OutOfArea() || sheep4.OutOfArea() || sheep5.OutOfArea()) {
+					alive = false;
+				}
+				
+				/*ball += elapsed * ball_velocity;
+				if (ball.x < -1.0f) ball_velocity.x = std::abs(ball_velocity.x);
+				if (ball.x >  1.0f) ball_velocity.x =-std::abs(ball_velocity.x);
+				if (ball.y < -1.0f) ball_velocity.y = std::abs(ball_velocity.y);
+				if (ball.y >  1.0f) ball_velocity.y =-std::abs(ball_velocity.y);*/
+			}
 
-		{ //update game state:
-			ball += elapsed * ball_velocity;
-			if (ball.x < -1.0f) ball_velocity.x = std::abs(ball_velocity.x);
-			if (ball.x >  1.0f) ball_velocity.x =-std::abs(ball_velocity.x);
-			if (ball.y < -1.0f) ball_velocity.y = std::abs(ball_velocity.y);
-			if (ball.y >  1.0f) ball_velocity.y =-std::abs(ball_velocity.y);
+			//draw output:
+			glClearColor(0.2f, 0.7f, 0.3f, 0.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+
+			{ //draw game state:
+				Draw draw;
+				draw.add_rectangle(glm::vec2(-0.95f, 0.95f), glm::vec2(0.95f, 0.90f), glm::u8vec4(0xB9, 0x7A, 0x57, 0xff));
+				draw.add_rectangle(glm::vec2(-0.95f, -0.90f), glm::vec2(0.95f, -0.95f), glm::u8vec4(0xB9, 0x7A, 0x57, 0xff));
+				draw.add_rectangle(glm::vec2(-0.95f, 0.90f), glm::vec2(-0.90f, -0.90f), glm::u8vec4(0xB9, 0x7A, 0x57, 0xff));
+				draw.add_rectangle(glm::vec2(0.90f, 0.90f), glm::vec2(0.95f, -0.95f), glm::u8vec4(0xB9, 0x7A, 0x57, 0xff));
+				draw.add_rectangle(sheep1.getLocation() + glm::vec2(-0.05f,-0.05f), sheep1.getLocation() + glm::vec2(0.05f, 0.05f), glm::u8vec4(0xff, 0xff, 0xff, 0xff));
+				draw.add_rectangle(sheep2.getLocation() + glm::vec2(-0.05f,-0.05f), sheep2.getLocation() + glm::vec2(0.05f, 0.05f), glm::u8vec4(0xff, 0xff, 0xff, 0xff));
+				draw.add_rectangle(sheep3.getLocation() + glm::vec2(-0.05f,-0.05f), sheep3.getLocation() + glm::vec2(0.05f, 0.05f), glm::u8vec4(0xff, 0xff, 0xff, 0xff));
+				draw.add_rectangle(sheep4.getLocation() + glm::vec2(-0.05f,-0.05f), sheep4.getLocation() + glm::vec2(0.05f, 0.05f), glm::u8vec4(0xff, 0xff, 0xff, 0xff));
+				draw.add_rectangle(sheep5.getLocation() + glm::vec2(-0.05f,-0.05f), sheep5.getLocation() + glm::vec2(0.05f, 0.05f), glm::u8vec4(0xff, 0xff, 0xff, 0xff));
+				draw.add_rectangle(mouse + glm::vec2(-0.05f,-0.05f), mouse + glm::vec2(0.05f, 0.05f), glm::u8vec4(0x00, 0x00, 0x00, 0xff));
+				draw.draw();
+			}
+
+
+			SDL_GL_SwapWindow(window);
 		}
-
-		//draw output:
-		glClearColor(0.0, 0.0, 0.0, 0.0);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-
-		{ //draw game state:
-			Draw draw;
-			draw.add_rectangle(mouse + glm::vec2(-0.1f,-0.1f), mouse + glm::vec2(0.1f, 0.1f), glm::u8vec4(0xff, 0xff, 0x00, 0xff));
-			draw.add_rectangle(ball + glm::vec2(-0.05f,-0.05f), ball + glm::vec2(0.05f, 0.05f), glm::u8vec4(0xff, 0x00, 0xff, 0xff));
-			draw.draw();
+		else {
+			sheep1.initSheep();
+			sheep2.initSheep();
+			sheep3.initSheep();
+			sheep4.initSheep();
+			sheep5.initSheep();
 		}
-
-
-		SDL_GL_SwapWindow(window);
 	}
 
 
